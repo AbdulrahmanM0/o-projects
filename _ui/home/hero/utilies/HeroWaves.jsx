52f4de1bd6ebf6a1f9944abe3282be90c9e1218a
Abdulrahman Mohammed <gabdulrahman.mohammed@gmail.com>
2026-03-03 11:05:21 +0200

    comments the silk

--- _ui/home/hero/utilies/HeroWaves.jsx
+++ _ui/home/hero/utilies/HeroWaves.jsx
@@ -1,15 +1,15 @@
-import Silk from 'react-silk';
+// import Silk from 'react-silk';
 
 export default function HeroWaves() {
   return (
     <div style={{ width: '1080px', height: '1080px', position: 'relative' }}>
-      <Silk
+      {/* <Silk
         speed={7.3}
         scale={1}
         color="#7B7481"
         noiseIntensity={2.6}
         rotation={2.2}
-      />
+      /> */}
     </div>
   )
 }
